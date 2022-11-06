78df6a374bcc562b8c6b1e2f2a8e7ee22c16e9af
Hallanzinho <112670402+Hallanzinho@users.noreply.github.com>
2022-11-06 20:19:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            int rg;
+            int cpf;
+            int nascimento;
+            string genero;
+            string etnia;
+            string endereço;
+            int numero;
+            int cep;
+            int telefone;
+            string experiencia;
+            string formação;
+            string carta;
+            string nome;
+
+            Console.WriteLine("diite seu nome: ");
+        nome: Console.ReadLine();
+
+            Console.WriteLine("digite sua idade: ");
+        idade: Console.ReadLine();
+
+            Console.WriteLine("digite seu rg: ");
+        rg: Console.ReadLine();
+
+            Console.WriteLine("digite seu cpf: ");
+        cpf: Console.ReadLine();
+
+            Console.WriteLine("data de nascimento: ");
+        nascimento: Console.ReadLine();
+
+            Console.WriteLine("genero: ");
+        genero: Console.ReadLine();
+
+            Console.WriteLine("digite seu endereço: ");
+        endereço: Console.ReadLine();
+
+            Console.WriteLine("digite o numero do seu endereço: ");
+        numero: Console.ReadLine();
+
+            Console.WriteLine("digite seu cep: ");
+        cep: Console.ReadLine();
+
+            Console.WriteLine("digite seu numero de celular: ");
+        telefone: Console.ReadLine();
+
+            Console.WriteLine("escreva sua exeperiencia profissional: ");
+        experiencia: Console.ReadLine();
+
+            Console.WriteLine("qual sua formação: ");
+        formação: Console.ReadLine();
+
+            Console.WriteLine("carta de apresentação: ");
+        carta: Console.ReadLine();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
